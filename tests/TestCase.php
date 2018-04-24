a29f418035cea2ad216aab5df6e67a2bed19d839
Bao Bao <roanvanbao@gmail.com>
2018-04-24 11:20:58 +0700

    Travis and add demo test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,17 @@
 <?php
 
-namespace Pingecom\Tests;
+namespace Pingecom\Tabler\Tests;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    /**
+     * Get TablerServiceProvider package service provider.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    public function getPackageProviders($app)
+    {
+        return ['Pingecom\Tabler\Providers\TablerServiceProvider'];
+    }
 }
---
+++ tests/Unit/DemoTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pingecom\Tabler\Tests\Unit;
+
+use Pingecom\Tabler\Tests\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function testDemo() {
+        $this->assertTrue(false);
+    }
+}
